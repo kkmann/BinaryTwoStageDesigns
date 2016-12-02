0c3e1b6f2fd24163c09b6e5bd565a480855def4e
Kevin Kunzmann <kevinkunzmann@gmx.net>
2016-12-02 18:15:35 +0100

    minors

--- src/ConfidenceInterval.jl
+++ src/ConfidenceInterval.jl
@@ -1,12 +1,16 @@
 abstract BinaryTwoStageDesignConfidenceInterval
 
+# these two enable array broadcasting of all methods!
+Base.size(::BinaryTwoStageDesignConfidenceInterval) = ()
+Base.getindex(ci::BinaryTwoStageDesignConfidenceInterval, i) = ci
+
 function limits{T<:Integer}(ci::BinaryTwoStageDesignConfidenceInterval, x1::T, x2::T)
     error("confidence interval is not implemented!")
 end
 
 function getConfidence(ci::BinaryTwoStageDesignConfidenceInterval)
     try
-        return estimator.confidence
+        return ci.confidence
     catch
         error("not implemented")
     end
